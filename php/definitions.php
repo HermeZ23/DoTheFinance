3f5cd2477c88408041a90dfe0415b3a3d9ab91ce
Pavel <pavel@dahncke.net>
2012-12-01 23:39:16 +0100

    jquery added

---
+++ php/definitions.php
@@ -0,0 +1,11 @@
+<?php 
+
+//Database stuff
+define("DB", "finance"); //DB name
+define("DBUSER", "financeDB"); //DB user
+define("DBPASS", "Password11"); //DB password
+define("DBPATH", "127.0.0.1"); //DB path
+
+
+
+ ?>
